28432af13813518f8b6cd4650b39597d5a780952
CreativeGP <malicktafaminecraft@gmail.com>
2018-03-05 23:02:59 +0900

    Support deleting and checking task

--- src/pages/Tasks.js
+++ src/pages/Tasks.js
@@ -13,19 +13,44 @@ import Title from '../components/Title'
 
 /**
  * 
- * @prop {Object} title   
+ * @prop {Object} title 
  */
-const TaskListItem = props => {
-    return (
-        <li className="list-group-item">
-            {props.task.title}
-            <small className="float-right text-muted">追加日：{dateFormat.format(new Date(props.task.add), 'MM/dd/yyyy')}</small>
-            {(()=>{
-                 if (props.task.deadline)
-                     return <p><small className="float-right text-muted">締め切り：{props.task.deadline}</small></p>
-            })()}
-        </li>
-    )
+class TaskListItem extends React.Component {
+
+    constructor (props) {
+        super(props)
+
+        this.state = { done: false }
+
+        this.toggleDone = this.toggleDone.bind(this)
+    }
+
+    toggleDone () {
+        this.setState({ done: !this.state.done })
+    }
+
+    render () {
+        return (
+            <li className={"list-group-item container " + (this.state.done?'done':'')}>
+                <div className="row">
+                <div className="col-sm-1">
+                    <p className="clickable text-muted" onClick={e=> this.props.handleDelete(this.props.task)}>×</p>
+                </div>
+                <div className="col-sm-1">
+                    <p className="clickable text-muted" onClick={e=> this.toggleDone()}>✓</p>
+                </div>
+                <div className="col-sm-10">
+                    {this.props.task.title}
+                    <small className="float-right text-muted">追加日：{dateFormat.format(new Date(this.props.task.add), 'MM/dd/yyyy')}</small>
+                    {(()=>{
+                        if (this.props.task.deadline)
+                            return <p><small className="float-right text-muted">締め切り：{this.props.task.deadline}</small></p>
+                    })()}
+                </div>
+                </div>
+            </li>
+        )
+    }
 }
 
 
@@ -70,7 +95,7 @@ class AddTaskModal extends React.Component {
                     <div className="modal-content">
                         <div className="modal-header">
                             <h4 className="modal-title">新しいタスクを追加</h4>
-                            <button type="button" className="close" data-dismiss="modal">&times;</button>
+                            <button type="button" className="close" data-dismiss="modal">×</button>
                         </div>
                         <div className="modal-body">
                             <form>
@@ -106,6 +131,66 @@ class AddTaskModal extends React.Component {
 
 /**
  * @class
+ * @prop {Boolean} show Whether this is expected to be shown
+ * @prop {Function} handleClose A handler to catch modal closing
+ * @prop {Function} onFinishRendering A handler to catch a time modal finish rendering 
+ * @prop {Function} switchPageCallback - a page switching function that is a method of App class
+*/
+class DeleteTaskModal extends React.Component {
+
+    constructor (props, context) {
+        super(props, context)
+
+        this.state = { id: '' }
+
+        this.deleteTask = this.deleteTask.bind(this)
+        this.changeId = this.changeId.bind(this)
+        this.componentDidUpdate = this.componentDidUpdate.bind(this)
+    }
+
+    changeId (id) {
+        this.setState({ id: id })
+    }
+
+    componentDidUpdate () {
+        this.props.onFinishRendering()
+    }
+
+    deleteTask (id) {
+        let data = JSON.parse(localStorage.tasks)
+        delete data[id]
+        localStorage.tasks = JSON.stringify(data)
+    
+        this.props.switchPageCallback('tasks')  // redraw
+    }
+    
+    render () {
+        if (this.state.id === '') return <div style={{display:'none'}}></div>
+        let data = JSON.parse(localStorage.tasks)
+        if (!(this.state.id in data)) return <div style={{display:'none'}}></div>
+        
+        return (
+            <div className="modal fade" id="deleteModal">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h4 className="modal-title">{data[this.state.id].title}を削除</h4>
+                            <button type="button" className="close" data-dismiss="modal">×</button>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal">キャンセル</button>
+                            <button type="button" className="btn btn-primary" data-dismiss="modal" onClick={e=> this.deleteTask(this.state.id)}>削除</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+/**
+ * @class
  * @prop {Function} switchPageCallback - a page switching function that is a method of App class
 */
 export default class Tasks extends React.Component {
@@ -114,6 +199,7 @@ export default class Tasks extends React.Component {
         super(props)
 
         this.showModal = this.showModal.bind(this)
+        this.showDeleteModal = this.showDeleteModal.bind(this)
         this.handleModalClose = this.handleModalClose.bind(this)
         
         this.state = { isModalVisible: false }
@@ -121,6 +207,10 @@ export default class Tasks extends React.Component {
 
     showModal () {
         $('#myModal').modal('show')
+    }
+
+    showDeleteModal () {
+        $('#deleteModal').modal('show')
     }
 
     handleModalClose () {
@@ -156,13 +246,15 @@ export default class Tasks extends React.Component {
                                     </li>
                                     {hash_get_values(JSON.parse(localStorage.tasks))
                                         .sort((a, b) => (new Date(b.add))-(new Date(a.add)))
-                                        .map((task)=> <TaskListItem key={task.id} task={task} />)}
+                                        .map((task)=> <TaskListItem key={task.id} task={task} handleDelete={task=> this.delete_task_modal.changeId(task.id)}/>)}
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+
                 <AddTaskModal switchPageCallback={this.props.switchPageCallback} />
+                <DeleteTaskModal switchPageCallback={this.props.switchPageCallback} onFinishRendering={()=> this.showDeleteModal()} ref={instance=> this.delete_task_modal = instance} />
             </div>
         )
     }
